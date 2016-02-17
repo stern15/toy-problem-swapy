1db5bfe8a419c07f1ccc2e8ae908c4fd3827d815
stern15 <ssemasuka@gmail.com>
2016-02-17 10:20:22 +0300

    swap lower and upper case in python

---
+++ swapie-own.py
@@ -0,0 +1,10 @@
+def main():
+	#def SwapCase(str):
+		word="Hello Everyone"
+		for x in word:
+			if x == x.upper():
+				print(x.lower(),end="")
+			else: 
+				print(x.upper(),end="")
+
+if __name__=="__main__":main()
